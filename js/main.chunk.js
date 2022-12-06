3d9f46611fef2cb8694ee34b1cb27762d6a88631
Thiago Novaes <54462424+thiagonovaess@users.noreply.github.com>
2022-12-06 19:58:02 -0300

    Update main.chunk.js

--- js/main.chunk.js
+++ js/main.chunk.js
@@ -533,7 +533,7 @@
             void 0 === f
               ? void 0
               : f.swarmUrl) || "wss://eu-swarm-springre.betconstruct.com/",
-          CASINO_URL: "https://api.7games.bet/casino/mirror/",
+          CASINO_URL: "https://api.7games.bet/casino/",
           JACKPOT_URL: "wss://rgs-wss.betconstructapps.com/jackpot",
           GEO_LINK_URL:
             (null === (d = window) ||
