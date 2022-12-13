fb770c1a98164b9407b8c3d1a5d83613a59de220
wellington <wba@cin.ufpe.br>
2022-12-13 11:12:18 -0300

    Update stop/start loading

--- js/beforeMount.js
+++ js/beforeMount.js
@@ -1,35 +1,18 @@
-var bodyDisplay = [];
-
-function hideElement (element) {
-    let oldDisplay = element.style.display;
-    element.style.display = "none";
-    return oldDisplay;
-}
-
 function startLoading () {
     let rawHtml = document.createElement("div");
     rawHtml.id = "loading-wrapper";
     rawHtml.innerHTML = `
-    <img src="https://static.7games.bet/images/loading.gif"/>
+        <img src="https://static.7games.bet/images/loading.gif"/>
     `;
-    let children = document.body.children;
-    for (let i = 0; i < children.length; i++) {
-        bodyDisplay.push(hideElement(children[i]));
-    }
     document.body.appendChild(rawHtml);
+    document.body.classList.add("loading-body");
 }
 
 
 function stopLoading () {
-    let loadingElm = document.getElementById("loading-wrapper");
-    if (loadingElm) {
-        document.body.removeChild(loadingElm);
-        let children = document.body.children;
-        for (let i = 0; i < children.length; i++) {
-            children[i].style.display = bodyDisplay[i];
-        }
-    }
+    document.getElementById("loading-wrapper")?.remove();
+    document.body.classList.remove("loading-body");
 }
 
-// startLoading();
-// setInterval("stopLoading()", 1100);
+startLoading();
+setInterval(stopLoading, 2000);
